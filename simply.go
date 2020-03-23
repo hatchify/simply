069f49ccb61e730e33080169c098aa5f89daec60
Lytic <d.halman@gmail.com>
2020-03-23 13:11:22 -0700

    gomu: Deploy local changes before incrementing version from v0.0.3

--- simply.go
+++ simply.go
@@ -26,6 +26,8 @@ type Simply struct {
 	result Result
 }
 
+var funcs = map[string]interface{}{}
+
 // Test returns a new instance of a test
 func Test(name string, context *testing.T) (test *Simply) {
 	var t Simply
@@ -37,9 +39,23 @@ func Test(name string, context *testing.T) (test *Simply) {
 		_, file := path.Split(abs)
 		t.callingLine = fmt.Sprintf("%s:%d", file, line)
 		t.callingFunc = fmt.Sprintf("%s::%s", context.Name(), name)
+
+		if _, ok := funcs[context.Name()]; !ok {
+			funcs[context.Name()] = nil
+			fmt.Println("\n" + context.Name())
+		}
 	}
 
 	return &t
+}
+
+// TestTarget returns a new instance of a test with target set
+func TestTarget(target interface{}, name string, context *testing.T) (test *Simply) {
+	t := Test(name, context)
+	t.target = stringify(target)
+	t.result.Status = PendingComparison
+
+	return t
 }
 
 // GetStatus returns the current state of the test execution
@@ -47,17 +63,17 @@ func (t *Simply) GetStatus() Status {
 	return t.result.Status
 }
 
-// Expects begins a test with a target value to check
+// Target begins a test with a target value to check
 // Returns a reference to test for chaining convenience
-func (t *Simply) Expects(target interface{}) *Simply {
+func (t *Simply) Target(target interface{}) *Simply {
 	t.result.Status = PendingComparison
 	t.target = stringify(target)
 	return t
 }
 
-// ToEqual ends a test with an expected value to compare to target
+// Equals ends a test with an expected value to compare to target
 // Returns a reference to test Result for validation
-func (t *Simply) ToEqual(val interface{}) *Result {
+func (t *Simply) Equals(val interface{}) *Result {
 	t.expected = stringify(val)
 	if t.target == t.expected {
 		t.handlePass()
@@ -75,7 +91,7 @@ func (t *Simply) handlePass() {
 	t.Validate = t.reportSuccess
 
 	t.result.Status = PassPendingValidation
-	t.result.output = fmt.Sprintf("    %s: %s - Passed!", t.callingLine, t.callingFunc)
+	t.result.output = fmt.Sprintf("    %s - Passed!", t.Name)
 }
 
 func (t *Simply) handleFail() {
@@ -83,7 +99,7 @@ func (t *Simply) handleFail() {
 	// t.Validate() should handle failures using default stdlib testing
 
 	t.result.Status = FailPendingValidation
-	t.result.output = fmt.Sprintf("%s - Failed! Expected <%+v> but got: <%+v>", t.callingFunc, t.expected, t.target)
+	t.result.output = fmt.Sprintf("%s - Failed! Expected <%+v> but got: <%+v>", t.Name, t.expected, t.target)
 }
 
 func (t *Simply) reportSuccess(a ...interface{}) {
