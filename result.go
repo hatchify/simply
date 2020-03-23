b1c74a7ae7e3896a3d2572cb8ecadde898a9043f
Lytic <d.halman@gmail.com>
2020-03-23 12:08:28 -0700

    gomu: Deploy local changes before incrementing version from v0.0.2

--- result.go
+++ result.go
@@ -1,11 +1,46 @@
 package simply
 
+// Status represents the current state of the test and result
+type Status int
+
+const (
+	// PendingExpects status means test has not yet set a target value
+	PendingExpects = iota
+	// PendingComparison status means test has not yet compared the expected value
+	PendingComparison
+
+	// FailPendingValidation status means the test has failed but not yet validated
+	FailPendingValidation
+	// PassPendingValidation status means the test passed but not yet validated
+	PassPendingValidation
+
+	// Failed means test was validated and failed
+	Failed
+	// Passed means test was validated and succeeded
+	Passed
+)
+
+// Result represents a test's completion status
 type Result struct {
-	Success  bool
-	Complete bool
-	output   string
+	Status Status
+
+	output string
 }
 
 func (r Result) String() string {
-	return r.output
+	switch r.Status {
+	case PassPendingValidation:
+		// Test just validated and passed
+		r.Status = Passed
+		return r.output
+	case FailPendingValidation:
+		// Test just validated and failed
+		r.Status = Failed
+		return r.output
+	}
+
+	var comparable interface{}
+	comparable = stringify(&r)
+
+	return comparable.(string)
 }
--- simply.go
+++ simply.go
@@ -9,9 +9,12 @@ import (
 
 type comparable string
 
-// Simply is a test context returned from simply.Test(*testing.T, Name)
+// Simply is a test context returned from simply.Test(Name, *testing.T)
 type Simply struct {
-	Name     string
+	// Name provided at test initialization
+	Name string
+
+	// Test.Validate() parses the test Result and reports success/failure
 	Validate func(args ...interface{})
 
 	target   comparable
@@ -20,14 +23,15 @@ type Simply struct {
 	callingLine string
 	callingFunc string
 
-	result *Result
+	result Result
 }
 
-// Test returns a new instance of a test and a pointer to the eventual result
+// Test returns a new instance of a test
 func Test(name string, context *testing.T) (test *Simply) {
 	var t Simply
 	t.Name = name
 	t.Validate = context.Error
+	t.result.Status = PendingExpects
 
 	if _, abs, line, ok := runtime.Caller(1); ok {
 		_, file := path.Split(abs)
@@ -38,31 +42,50 @@ func Test(name string, context *testing.T) (test *Simply) {
 	return &t
 }
 
+// GetStatus returns the current state of the test execution
+func (t *Simply) GetStatus() Status {
+	return t.result.Status
+}
+
+// Expects begins a test with a target value to check
+// Returns a reference to test for chaining convenience
 func (t *Simply) Expects(target interface{}) *Simply {
+	t.result.Status = PendingComparison
 	t.target = stringify(target)
 	return t
 }
 
-func (t *Simply) success(a ...interface{}) {
-	fmt.Println(t.result)
-}
-
+// ToEqual ends a test with an expected value to compare to target
+// Returns a reference to test Result for validation
 func (t *Simply) ToEqual(val interface{}) *Result {
-	var result Result
-	t.result = &result
-
 	t.expected = stringify(val)
 	if t.target == t.expected {
-		// t.Validate should print success output :)
-		t.result.output = fmt.Sprintf("    %s: %s - Passed!", t.callingLine, t.callingFunc)
-		t.result.Success = true
-		t.Validate = t.success
+		t.handlePass()
+
 	} else {
-		// t.Validate should equal *testing.T.Errof(), which handles failures using default stdlib testing
-		t.result.output = fmt.Sprintf("%s - Failed! Expected <%+v> but got: <%+v>", t.callingFunc, t.expected, t.target)
+		t.handleFail()
 	}
 
-	t.result.Complete = true
+	return &t.result
+}
 
-	return t.result
+func (t *Simply) handlePass() {
+	// Set validation handler to success
+	// t.Validate() should print success output :)
+	t.Validate = t.reportSuccess
+
+	t.result.Status = PassPendingValidation
+	t.result.output = fmt.Sprintf("    %s: %s - Passed!", t.callingLine, t.callingFunc)
+}
+
+func (t *Simply) handleFail() {
+	// Validation handler should already be set to t.context.Error()
+	// t.Validate() should handle failures using default stdlib testing
+
+	t.result.Status = FailPendingValidation
+	t.result.output = fmt.Sprintf("%s - Failed! Expected <%+v> but got: <%+v>", t.callingFunc, t.expected, t.target)
+}
+
+func (t *Simply) reportSuccess(a ...interface{}) {
+	fmt.Println(t.result)
 }
